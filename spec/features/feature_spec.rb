e969925768e08ff9502b5a73841fb6094b8058ac
ComaToastUK <jesusmanyouatethedog@gmail.com>
2017-10-23 00:45:01 +0100

    moar tests

--- spec/features/feature_spec.rb
+++ spec/features/feature_spec.rb
@@ -7,16 +7,28 @@ feature 'it has a root page' do
   end
 end
 
-feature 'a user can post an image' do
-  scenario 'a user posts a picture of their cat' do
-    visit '/posts/new'
-    fill_in 'post[image]',   with: 'cat.jpeg'
-    fill_in 'post[caption]', with: 'cat <3'
-    submit_form
-    visit '/'
-    expect(page).to have_content 'cat <3'
+feature 'a user can sign up to instagrin' do
+  scenario 'a new user joins instagrin' do
+    visit '/users/sign_up'
+       fill_in 'user[user_name]', with: 'ComaToastUK'
+       fill_in 'user[email]', with: 'comatoast@testmail.com'
+       fill_in 'user[password]', with: 'password1', match: :first
+       fill_in 'user[password_confirmation]', with: 'password1'
+       first(:link, 'Sign up').click
+       expect(page).to have_content
   end
 end
+
+# feature 'a user can post an image' do
+#   scenario 'a user posts a picture of their cat' do
+#     visit '/posts/new'
+#     fill_in 'post[image]',   with: 'cat.jpeg'
+#     fill_in 'post[caption]', with: 'cat <3'
+#     submit_form
+#     visit '/'
+#     expect(page).to have_content 'cat <3'
+#   end
+# end
 
 feature 'a user can comment on an image' do
   scenario 'user wants to comment on the image of another user' do
@@ -24,11 +36,7 @@ feature 'a user can comment on an image' do
   end
 end
 
-feature 'a user can sign up to instagrin' do
-  scenario 'a new user joins instagrin' do
 
-  end
-end
 
 feature 'a user can comment on another users post' do
   scenario 'user1 wants to comment on user2 picture' do
@@ -39,9 +47,13 @@ end
 private
 
 def sign_up
-  visit 'users/new' do
-    fill_in ''
-  end
+  visit '/users/sign_up'
+     fill_in 'user[user_name]', with: 'ComaToastUK'
+     fill_in 'user[email]', with: 'comatoast@testmail.com'
+     fill_in 'user[password]', with: 'password1', match: :first
+     fill_in 'user[password_confirmation]', with: 'password1'
+     first(:link, 'Sign up').click
+     expect(page).to have_content
 end
 
 def submit_form
